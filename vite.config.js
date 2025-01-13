85b50999567d5fe29ae51801d8f5e64bf2fefac0
Sukhpal25 <SukhpalSKang25@gmail.com>
2025-01-12 19:55:25 -0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/financial-data-app/', // Add this line - match your repo name
+  base: '/financial-data-app/index.html', // Add this line - match your repo name
 })
