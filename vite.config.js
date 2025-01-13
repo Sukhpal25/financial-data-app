0c7a3b9d528577ff455fd9cd4a4de7ec3f9a697d
Sukhpal25 <SukhpalSKang25@gmail.com>
2025-01-12 19:51:44 -0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/financial-data-app/', // Add this line - match your repo name
 })
